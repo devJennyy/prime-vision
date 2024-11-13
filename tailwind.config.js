bf774114d70d6614363d0f11ec4282d018b2f2e2
Jenny Pieloor <devjenny.official@gmail.com>
2024-11-13 22:11:34 +0800

    Test dark mode default settings.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  darkMode: 'selector',
+  darkMode: 'class',
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
       '2xl': '1440px',
       '3xl': '1920px',
     },
